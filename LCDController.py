73e05167912c48490702057d439a6e9972103f31
Marcel Jira <pi@flushfm.(none)>
2015-05-24 16:44:28 +0000

    move completed

---
+++ LCDController.py
@@ -0,0 +1,103 @@
+__author__ = 'marcel'
+
+from i2c_lcd_patched import i2c_lcd
+import threading
+
+
+class LCD:
+	def __init__(self, lines, columns):
+		# init lcd
+		self.lcd = i2c_lcd(0x27, 1, 2, 1, 0, 4, 5, 6, 7, 3)
+
+		# Lock
+		self.lock = threading.RLock()
+
+		# how many lines and columns
+		self.lines = lines
+		self.columns = columns
+		# array containing the lines
+		self.lineContainer = dict()
+
+		# position
+		self.cursor = (0, 0)
+
+		# remove cursor
+		self.lcd.command(self.lcd.CMD_Display_Control | self.lcd.OPT_Enable_Display)
+		# turn backlingt on
+		self.backlight(True)
+		# state
+		self.suspend = False
+
+	def backlight(self, stat):
+		if stat:
+			self.lock.acquire()
+			try:
+				self.lcd.backLightOn()
+			finally:
+				self.lock.release()
+		else:
+			self.lock.acquire()
+			try:
+				self.lcd.backLightOff()
+			finally:
+				self.lock.release()
+
+	def set_line(self, name, lcd_line):
+		if (lcd_line.line_number <= 0) | (lcd_line.line_number > self.lines):
+			raise ValueError("Not a valid line number!")
+		else:
+			# check if line is reserved
+			for cur_line in self.lineContainer.values():
+				if cur_line.line_number == lcd_line.line_number:
+					raise ValueError("Line already in use!")
+
+			# else go ahead
+			self.lineContainer[name] = lcd_line
+
+		return lcd_line
+
+	def remove_line(self, line):
+		self.lineContainer[line].set_text(self.columns * " ")
+
+		del self.lineContainer[line]
+
+	def set_position(self, line, column):
+		self.lock.acquire()
+		try:
+			self.lcd.setPosition(line, column)
+
+			# update current position
+			self.cursor = (line, column)
+
+		finally:
+			self.lock.release()
+
+	def write_char_at(self, line, column, char):
+			self.lock.acquire()
+			try:
+				if (self.cursor[0] != line) | (self.cursor[1] != column):
+					self.set_position(line, column)
+				self.lcd.writeChar(char)
+				self.cursor = (line, column + 1)
+			finally:
+				self.lock.release()
+
+	def standby(self):
+		if not self.suspend:
+			self.suspend = True
+
+			for line in self.lineContainer.values():
+				line.lock.acquire()
+
+			self.lcd.command(self.lcd.CMD_Clear_Display)
+			self.backlight(False)
+
+	def resume(self):
+		if self.suspend:
+			self.suspend = False
+
+			for line in self.lineContainer.values():
+				line.lock.release()
+				line.resume()
+
+			self.backlight(True)
---
+++ LineController.py
@@ -0,0 +1,248 @@
+__author__ = 'marcel'
+
+import threading
+
+
+class LCDLine(threading.Thread):
+	import time
+	import math
+
+	def __init__(self, lcd, line_number, **kwargs):
+		super().__init__()
+		self.lcd = lcd
+		self.line_number = line_number
+
+		# kwargs
+		self.refresh_interval = 0.5
+		self.step_interval = 0.5
+		self.start_duration = 3.0
+		self.end_duration = 5.0
+		self.columns = self.lcd.columns
+
+		for key, value in kwargs.items():
+			setattr(self, key, value)
+
+		# current text
+		self.text = ""
+		# current position
+		self.text_pos = 0
+		# marquee necessary?
+		self.do_marquee = False
+		# last marquee step
+		self.last_marquee_step = None
+		# current text
+		self._current_text = self.columns * " "
+		# Status
+		self.lock = threading.RLock()
+
+	def run_every(self):
+		self.lock.acquire()
+		try:
+			self._time_for_marquee()
+			self._write_string()
+			threading.Timer(self.refresh_interval, self.run_every).start()
+		finally:
+			self.lock.release()
+
+	def resume(self):
+		self._current_text = self.columns * ' '
+		self.clear_text()
+
+	def format_text(self, text, align='l'):
+		if len(text) < self.columns:
+			if align == 'r':
+				return (self.columns - len(text)) * " " + text
+			elif align == 'c':
+				return self.math.floor((self.columns - len(text)) / 2) * " " + \
+				       text + self.math.ceil((self.columns - len(text)) / 2) * " "
+			else:
+				return text + (self.columns - len(text)) * " "
+		else:
+			return text
+
+	def set_text(self, text, align='l'):
+		self.text_pos = 0
+		self.last_marquee_step = self.time.time()
+
+		if len(text) > self.columns:
+			self.do_marquee = True
+			self.text = text
+		else:
+			self.do_marquee = False
+			# add the needed amount of whitespace
+			self.text = self.format_text(text, align)
+		self._write_string()
+
+	def set_text_right(self, text):
+		prev_text = self.text.rstrip(' ')
+
+		if len(prev_text) + len(text) > self.columns:
+			raise ValueError("Text too long, only " + self.columns + "characters.")
+		else:
+			self.text = prev_text + (' ' * (self.columns - len(prev_text) - len(text))) + text
+
+	def set_text_left(self, text):
+		prev_text = self.text.lstrip(' ')
+		if len(prev_text) + len(text) > self.columns:
+			raise ValueError("Text too long, only " + self.columns + "characters.")
+		else:
+			self.text = text + (' ' * (self.columns - len(prev_text) - len(text))) + prev_text
+
+	def clear_text(self):
+		self.text = ""
+		self.text_pos = 0
+
+	def _set_position(self, column):
+		if (column >= 0) & (column < self.columns):
+			self.lcd.set_position(self.line_number, column)
+		else:
+			raise ValueError("Column " + column + " does not exist.")
+
+	def _time_for_marquee(self):
+		if (self.last_marquee_step is not None) & self.do_marquee:
+			if (self.text_pos == 0) & (self.time.time() - self.last_marquee_step >= self.start_duration):
+				self._marquee_step()
+			elif (self.text_pos == (len(self.text) - self.columns)) & \
+					(self.time.time() - self.last_marquee_step >= self.end_duration):
+				self._marquee_step()
+			elif (self.text_pos > 0) & (self.text_pos < len(self.text) - self.columns) & \
+					(self.time.time() - self.last_marquee_step >= self.step_interval):
+				self._marquee_step()
+
+	def _marquee_step(self):
+		if self.do_marquee:
+			self.text_pos += 1
+			if self.text_pos >= len(self.text) - self.columns + 1:
+				self.text_pos = 0
+			self.last_marquee_step = self.time.time()
+
+	def _write_string(self):
+		text_to_set = self.text[self.text_pos:(self.columns + self.text_pos)]
+		for i in range(0, self.columns):
+			if text_to_set[i] != self._current_text[i]:
+				self.lcd.write_char_at(self.line_number, i, text_to_set[i])
+
+		self._current_text = text_to_set
+
+
+class TimeLine(LCDLine):
+
+	def __init__(self, lcd, line_number, **kwargs):
+		super().__init__(lcd, line_number, **kwargs)
+
+		self.start_time = self.time.time()
+
+		self.update_time()
+
+	def run_every(self):
+		self.lock.acquire()
+		try:
+			self.update_time()
+			super().run_every()
+		finally:
+			self.lock.release()
+
+	def resume(self):
+		super().resume()
+		self.start_time = self.time.time()
+
+	def update_time(self):
+		self.clear_text()
+
+		self.set_text_left(self.time.strftime('%H:%M:%S'))
+
+		self.set_text_right(self.time.strftime("%H:%M:%S", self.time.gmtime(self.time.time() - self.start_time)))
+
+
+class TextLine(LCDLine):
+	def __init__(self, lcd, line_number, **kwargs):
+		self.align = 'l'
+		self.query_info_interval = 5.0
+		self.last_update = None
+		super().__init__(lcd, line_number, **kwargs)
+
+	def run_every(self):
+		self.lock.acquire()
+		try:
+			if self.last_update is None:
+				self.update_text()
+			elif self.time.time() - self.last_update >= self.query_info_interval:
+				self.update_text()
+
+			super().run_every()
+		finally:
+			self.lock.release()
+
+	def resume(self):
+		super().resume()
+		self.last_update = None
+
+	def update_text(self):
+		raise NotImplementedError("This is an abstract class (update_text() must be implemented in subclass)")
+
+
+class MPDLine(TextLine):
+	from mpd import MPDClient
+
+	def __init__(self, lcd, line_number, key, **kwargs):
+		super().__init__(lcd, line_number, **kwargs)
+
+		self.client = self.MPDClient()
+		self.client.timeout = 10
+		self.client.connect("localhost", 6600)
+		self.key = key
+
+	def update_text(self):
+		if self.key in self.client.currentsong():
+			new_text = self.format_text(self.client.currentsong()[self.key], self.align)
+		else:
+			new_text = "NO TEXT"
+
+		if self.text != new_text:
+			self.set_text(new_text, self.align)
+			self.text_pos = 0
+
+		self.last_update = self.time.time()
+
+
+class FetchLine(TextLine):
+	from urllib import request
+
+	def __init__(self, lcd, line_number, url, **kwargs):
+		super().__init__(lcd, line_number, **kwargs)
+		self.query_info_interval = 60.0
+		self.decode = 'latin1'
+
+		self.url = url
+		self.req = self.request.Request(url)
+
+
+		self.fetcher = self.Fetcher(self.req, self.decode)
+		self.fetcher.start()
+
+	class Fetcher(threading.Thread):
+		from urllib import request, error
+
+		def __init__(self, req, decode):
+			super().__init__()
+
+			self.text = "NO DATA"
+
+			self.req = req
+			self.decode = decode
+
+		def run(self):
+			try:
+				response = self.request.urlopen(self.req)
+				self.text = response.read().decode(self.decode)
+			except (self.error.HTTPError, self.error.URLError) as e:
+				print(e)
+
+	def update_text(self):
+		new_text = self.format_text(self.fetcher.text, self.align)
+
+		if self.text != new_text:
+			self.set_text(new_text, self.align)
+
+		self.fetcher.run()
+		self.last_update = self.time.time()
